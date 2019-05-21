69681f1946410963404b78b404c6b52a170dc70a
Nguyen Quang An <ngquangan@gmail.com>
2019-05-22 01:08:47 +0700

    Config react native firebase

--- AdmodExample/ios/AdmodExample/AppDelegate.m
+++ AdmodExample/ios/AdmodExample/AppDelegate.m
@@ -11,6 +11,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Firebase.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -20,6 +22,8 @@
                                                    moduleName:@"AdmodExample"
                                             initialProperties:nil];
 
+  [FIRApp configure];
+  
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
